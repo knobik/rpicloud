64ee380e8d58921fd618094116da3ee4708c66d7
Knobik <knobiks@gmail.com>
2020-05-29 15:16:48 +0200

    test

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m';
 NC='\033[0m'
-DOWNLOAD_URL="https://downloads.raspberrypi.org/raspbian_lite_latest"
+DOWNLOAD_URL="https://downloads.raspberrypi.org/raspios_lite_armhf_latest"
 
 echo -e "${GREEN}This script will download the latest raspbian image, mount it and copy its files as a base for netboot, next it will build and run the docker image.${NC}"
+
+if sudo apt-get -qq install curl unzip; then
+    echo "Dependencies installed"
+else
+    echo "Error installing dependencies"
+fi
 
 echo -e "${GREEN}Downloading from ${NC}${DOWNLOAD_URL}"
 curl -fSL ${DOWNLOAD_URL} -o /tmp/raspbian.zip
@@ -16,32 +22,32 @@ rm /tmp/raspbian.zip
 IMAGE_FILENAME=$(find /tmp/raspbian/ -type f -name "*.img")
 
 echo -e "${GREEN}Fetching partition offsets.${NC}"
-OFFSET_FAT32=$(($(fdisk -l ${IMAGE_FILENAME} | grep ^${IMAGE_FILENAME}1 | awk -F" " '{ print $2 }') * 512))
+OFFSET_FAT32=$(($(fdisk -l "${IMAGE_FILENAME}" | grep ^"${IMAGE_FILENAME}"1 | awk -F" " '{ print $2 }') * 512))
 echo -e "${GREEN}Offset for FAT32 partition is: ${NC}${OFFSET_FAT32}"
 
-OFFSET_EXT4=$(($(fdisk -l ${IMAGE_FILENAME} | grep ^${IMAGE_FILENAME}2 | awk -F" " '{ print $2 }') * 512))
+OFFSET_EXT4=$(($(fdisk -l "${IMAGE_FILENAME}" | grep ^"${IMAGE_FILENAME}"2 | awk -F" " '{ print $2 }') * 512))
 echo -e "${GREEN}Offset for EXT4 partition is: ${NC}${OFFSET_EXT4}"
 
 echo -e "${GREEN}Mounting boot partition${NC}"
 BOOT_PATH=/mnt/img/boot
 sudo mkdir -p ${BOOT_PATH}
-sudo mount -v -o offset=${OFFSET_FAT32} -t vfat ${IMAGE_FILENAME} ${BOOT_PATH}
-sudo cp -a ${BOOT_PATH} ./baseImage/boot
-sudo umount ${BOOT_PATH}
+sudo mount -v -o offset=${OFFSET_FAT32} -t vfat "${IMAGE_FILENAME}" ${BOOT_PATH}
+sudo cp -a ${BOOT_PATH} ./baseImage/
+sudo umount -fl ${BOOT_PATH}
 
 echo -e "${GREEN}Mounting root partition${NC}"
 ROOT_PATH=/mnt/img/root
 sudo mkdir -p ${ROOT_PATH}
-sudo mount -v -o offset=${OFFSET_EXT4} -t ext4 ${IMAGE_FILENAME} ${ROOT_PATH}
-sudo cp -a ${ROOT_PATH} ./baseImage/root
-sudo umount ${ROOT_PATH}
+sudo mount -v -o offset=${OFFSET_EXT4} -t ext4 "${IMAGE_FILENAME}" ${ROOT_PATH}
+sudo cp -a ${ROOT_PATH} ./baseImage/
+sudo umount -fl ${ROOT_PATH}
 
-echo -e "${GREEN}Buidling the docker image...${NC}"
+echo -e "${GREEN}Building the docker image...${NC}"
 docker-compose up -d --build
 docker-compose exec --user rpi rpi-cluster-pxe composer install -d /var/www/html/
 docker-compose exec --user rpi rpi-cluster-pxe composer setup -d /var/www/html/
 
 echo -e "${GREEN}Cleaning up...${NC}"
-rm ${IMAGE_FILENAME}
+rm "${IMAGE_FILENAME}"
 
 echo -e "${GREEN}Done!${NC}"
