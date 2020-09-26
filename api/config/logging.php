56aa4a068d6daece592e2a427cef5d0b66a8d8f1
knobik <knobiks@gmail.com>
2020-09-26 09:20:16 +0200

    tagging

--- api/config/logging.php
+++ api/config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['stderr'],
             'ignore_exceptions' => false,
         ],
 
