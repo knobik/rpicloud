59e6e510f21c1b96f918b1c95b3b9d7fed37aa63
Knobik <knobiks@gmail.com>
2020-11-22 13:31:14 +0100

    init the cloud-init

--- api/stubs/scripts/provision.sh
+++ api/stubs/scripts/provision.sh
@@ -2,6 +2,7 @@
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root (you can use sudo)" ; exit 1 ; fi
 
+# add system user
 if ! id -u __config.user__ > /dev/null 2>&1; then
     useradd __config.user__
     mkdir -p /home/__config.user__/.ssh
@@ -15,6 +16,11 @@ else
     echo "__config.user__ user aleady exists. Skipping..."
 fi;
 
+# set the authorized ssh key, system user should login without password
 curl -fsSL -X POST __url__/api/provision/register > /home/__config.user__/.ssh/authorized_keys
+
+# install cloud-init and disable it for time being
+apt update && apt install -y cloud-init
+touch /etc/cloud/cloud-init.disabled
 
 echo "Done."
